87a96c564b167075b37c3329eee6637a3a37ab1e
Gunnar van Remoortere <Gunnar.Remoortere@AOMV.local>
2022-10-18 14:40:27 +0200

    Finished NextJS starterproject

---
+++ pages/index.js
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+
+//fetches the data from the .md files using the code from ./lib/posts.js
+export async function getStaticProps() {
+  // Instead of the file system,
+  // fetch post data from an external API endpoint
+  //const res = await fetch('..');
+  //return res.json();
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          I am currently <u>learning</u> the ways of <b>Next.JS</b> so I can
+          start making my employer proud.
+        </p>
+        <p>
+          This is some kind of easy <b>Next.JS</b> practice project to make
+          myself familiar with the ways of <b>Next.JS</b>.
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog articles</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
